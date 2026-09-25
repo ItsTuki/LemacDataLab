397f11b955f9ac1f84655c5e9b5b7fb351dda51a
tuki <rar56721@gmail.com>
2026-09-24 23:18:30 -0300

    Se crea y compila main para mostrar menu login

--- app/src/main/java/com/losk/lemacdatalab/MainActivity.kt
+++ app/src/main/java/com/losk/lemacdatalab/MainActivity.kt
@@ -1,47 +1,32 @@
 package com.losk.lemacdatalab
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.losk.lemacdatalab.ui.screens.LoginScreen
 import com.losk.lemacdatalab.ui.theme.LemacDataLabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             LemacDataLabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginScreen(
+                        onLoginSuccess = {
+                            Toast.makeText(this, "¡Bienvenido a LemacDataLab!", Toast.LENGTH_SHORT).show()
+                        }
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LemacDataLabTheme {
-        Greeting("Android")
     }
 }
